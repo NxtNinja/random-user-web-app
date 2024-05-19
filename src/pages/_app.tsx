621eb0a9270db0f68c92d7a72d007966e9f3cc9b
NxtNinja <136341993+NxtNinja@users.noreply.github.com>
2024-05-19 21:02:06 +0530

    nextui and tanstack query setup done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({ subsets: ["latin"], weight: "400" });
+
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        <main className={raleway.className}>
+          <Component {...pageProps} />
+        </main>
+      </NextUIProvider>
+    </QueryClientProvider>
+  );
 }
